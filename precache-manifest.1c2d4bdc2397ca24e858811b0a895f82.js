c58bab7c6045dadde6f78389cb37818655547eb2
NazarYanchyshyn <yanchishin12@ukr.net>
2021-07-28 16:17:36 +0300

    Updates

---
+++ precache-manifest.1c2d4bdc2397ca24e858811b0a895f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eead651fcc753a6e3a3a0e05d2cee2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa11cc2dbb3045a25c62",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "a177f87b2deefea314bd",
+    "url": "./static/css/main.9f604ba9.chunk.css"
+  },
+  {
+    "revision": "fa11cc2dbb3045a25c62",
+    "url": "./static/js/2.ff0d155a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ff0d155a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a177f87b2deefea314bd",
+    "url": "./static/js/main.b62043f0.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
